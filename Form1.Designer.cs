044df4ff8cf9773324b259740815ce886e47c1d2
kurtreload <140906336+kurtreload@users.noreply.github.com>
2025-04-22 21:51:04 -0500

    primer update masivo. He cambiado la GUI a una simple.

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace LimboLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.btnJugar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnComprobar = new System.Windows.Forms.Button();
+            this.barraProgreso = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(608, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bienvenido a WOW Limbo";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStatus.Location = new System.Drawing.Point(12, 67);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(648, 157);
+            this.txtStatus.TabIndex = 1;
+            this.txtStatus.TextChanged += new System.EventHandler(this.txtStatus_TextChanged);
+            // 
+            // btnJugar
+            // 
+            this.btnJugar.Enabled = false;
+            this.btnJugar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJugar.Location = new System.Drawing.Point(666, 195);
+            this.btnJugar.Name = "btnJugar";
+            this.btnJugar.Size = new System.Drawing.Size(122, 58);
+            this.btnJugar.TabIndex = 2;
+            this.btnJugar.Text = "Jugar";
+            this.btnJugar.UseVisualStyleBackColor = true;
+            this.btnJugar.Click += new System.EventHandler(this.btnJugar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Enabled = false;
+            this.btnActualizar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActualizar.Location = new System.Drawing.Point(666, 131);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(122, 58);
+            this.btnActualizar.TabIndex = 3;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnComprobar
+            // 
+            this.btnComprobar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnComprobar.Location = new System.Drawing.Point(666, 67);
+            this.btnComprobar.Name = "btnComprobar";
+            this.btnComprobar.Size = new System.Drawing.Size(122, 58);
+            this.btnComprobar.TabIndex = 4;
+            this.btnComprobar.Text = "Comprobar";
+            this.btnComprobar.UseVisualStyleBackColor = true;
+            this.btnComprobar.Click += new System.EventHandler(this.btnComprobar_Click);
+            // 
+            // barraProgreso
+            // 
+            this.barraProgreso.Location = new System.Drawing.Point(12, 230);
+            this.barraProgreso.Name = "barraProgreso";
+            this.barraProgreso.Size = new System.Drawing.Size(648, 23);
+            this.barraProgreso.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 263);
+            this.Controls.Add(this.barraProgreso);
+            this.Controls.Add(this.btnComprobar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnJugar);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WOW Limbo Launcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Button btnJugar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnComprobar;
+        private System.Windows.Forms.ProgressBar barraProgreso;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LimboLauncher
+{
+    public partial class Form1 : Form
+    {
+        private List<PatchData> listaParches;
+        private string urlPatchlist = "https://limbo.org.pe/PatchHD/plist.txt";
+        private string urlParches = "https://limbo.org.pe/PatchHD/";
+        public Form1()
+        {
+            InitializeComponent();
+            listaParches = new List<PatchData>();
+            construirPatchlist();
+            //agrega el método de parchar todo al evento de click -> actualizar el cliente
+            btnActualizar.Click += async (sender, e) => await ParcharTodo();
+        }
+
+        private void btnComprobar_Click(object sender, EventArgs e)
+        {
+            //comprobar si el cliente está actualizado
+            if (necesitaUpdate())
+            {
+                btnActualizar.Enabled = true;
+            }
+            else
+            {
+                btnJugar.Enabled = true;
+            }
+        }
+        
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnJugar_Click(object sender, EventArgs e)
+        {
+            //abrir el wow y cerrar este programa
+            if (File.Exists("Wow.exe"))
+            {
+                Process.Start(new ProcessStartInfo("Wow.exe")
+                {
+                    UseShellExecute = true
+                });
+                //Application.Current.Shutdown(); // close launcher, maybe make this an option?
+                this.Close();
+            }
+            else
+            {
+                txtStatus.AppendText("Por favor ubica los archivos del parche en tu carpeta del WOW !!");
+                this.Close();
+            }
+        }
+        private void construirPatchlist()
+        {
+            if (!Directory.Exists("Data/temp"))
+                Directory.CreateDirectory("Data/temp");
+            if (File.Exists("Data/temp/plist.txt"))
+                File.Delete("Data/temp/plist.txt");
+            WebRequest request = WebRequest.Create(urlPatchlist);
+            try
+            {
+                txtStatus.AppendText("Obteniendo Patchlist...\r\n");
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                using (WebClient wc = new WebClient())
+                {
+                    wc.DownloadFile(new Uri(urlPatchlist), "Data/temp/plist.txt");
+                }
+                //listaParches = PreparePatchList("Data/temp/plist.txt");
+                txtStatus.AppendText("Patchlist descargado!\r\n");
+            }
+            catch(Exception)
+            {
+                txtStatus.AppendText("No se pudo obtener el Patchlist. Revise su conexión y el estado del servidor\r\n");
+            }
+        }
+        private bool necesitaUpdate()
+        {
+            bool necesidad = false;
+            int contar = 0;
+            txtStatus.AppendText("Comprobando....\r\n");
+            listaParches = PreparePatchList("Data/temp/plist.txt");
+            try
+            {
+                if (listaParches.Count > 0)
+                {
+                    for (int i = 0; i < listaParches.Count(); i++)
+                    {
+                        if (!File.Exists($"Data/{listaParches[i].Filename}"))
+                        {
+                            File.Create($"Data/{listaParches[i].Filename}").Close();//crea un archivo vacío si no existe
+                        }
+                        string localhash = string.Empty;
+                        using (MD5 crypto = MD5.Create())
+                        {
+                            FileStream fileStream = File.OpenRead($"Data/{listaParches[i].Filename}");
+                            localhash = BitConverter.ToString(crypto.ComputeHash(fileStream)).Replace("-", "").ToLowerInvariant();
+                            fileStream.Close();
+                        }
+                        if (!localhash.Equals(listaParches[i].Checksum)) {
+                            contar++;
+                        }
+                    }
+                    if (contar > 0)
+                    {
+                        necesidad = true;
+                        txtStatus.AppendText("NECESITA ACTUALIZAR!!\r\n");
+                    }
+                    else
+                    {
+                        txtStatus.AppendText("No necesita actualizar\r\n");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                txtStatus.AppendText("Fallo al leer el Patchlist!\r\n");
+            }
+            return necesidad;
+        }
+        private struct PatchData
+        {
+            public string Filename;
+            public string Checksum;
+        }
+        private List<PatchData> PreparePatchList(string ruta)
+        {
+            IEnumerable<string> lista = File.ReadLines(ruta);
+            listaParches = new List<PatchData>();
+            foreach (string parche in lista)
+            {
+                string[] data = parche.Split(' ');
+                listaParches.Add(new PatchData()
+                {
+                    Filename = data[0],
+                    Checksum = data[1]
+                });
+            }
+            return listaParches;
+        }
+        private async Task ParcharTodo()
+        {
+            txtStatus.AppendText("Iniciando parchado. Por favor no cierres el launcher ni inicies el juego!!\r\n");
+            try
+            {
+                for (int i = 0; i < listaParches.Count(); i++)
+                {
+                    string localhash = string.Empty;
+                    using (MD5 crypto = MD5.Create())
+                    {
+                        FileStream fileStream = File.OpenRead($"Data/{listaParches[i].Filename}");
+                        localhash = BitConverter.ToString(crypto.ComputeHash(fileStream)).Replace("-", "").ToLowerInvariant();
+                        fileStream.Close();
+                    }
+                    if (!localhash.Equals(listaParches[i].Checksum))
+                    {
+                        //parchar
+                        using (WebClient wc = new WebClient())
+                        {
+                            wc.DownloadProgressChanged += (s, e) => {
+                                barraProgreso.Value = e.ProgressPercentage;
+                            };
+                            txtStatus.AppendText($"Parchando {listaParches[i].Filename}\r\n");
+                            await DescargaUnoAUno(wc, $"{urlParches}{listaParches[i].Filename}", $"Data/{listaParches[i].Filename}");
+                            txtStatus.AppendText($"Parchado!! {listaParches[i].Filename}\r\n");
+                        }
+                    }
+                }
+                btnJugar.Enabled = true;
+            }
+            catch (Exception ex) {
+                while (ex != null)
+                {
+                    txtStatus.AppendText(ex.Message);
+                    ex = ex.InnerException;
+                }
+            }
+        }
+        private async Task DescargaUnoAUno(WebClient webClient, string archivo, string ruta)
+        {
+            await webClient.DownloadFileTaskAsync(new Uri(archivo), ruta);
+        }
+
+        private void txtStatus_TextChanged(object sender, EventArgs e)
+        {
+            txtStatus.SelectionStart = txtStatus.Text.Length;
+            txtStatus.ScrollToCaret();
+        }
+    }
+}
